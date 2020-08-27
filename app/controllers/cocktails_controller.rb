a03f04a609b670b7e8192fa27e230145f6225301
Tomine Odegard <tomineodegard@hotmail.no>
2020-08-27 21:36:34 +0200

    working on the front end, be patient with me

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -15,7 +15,7 @@ class CocktailsController < ApplicationController
   def create
     @cocktail = Cocktail.new(cocktail_params)
     @cocktail.save
-    redirect_to cocktails_path(@cocktail)
+    redirect_to cocktails_path
   end
 
 
